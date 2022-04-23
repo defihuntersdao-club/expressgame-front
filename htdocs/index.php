306f5267be09996e1ef0208d6554d696e2bf8a94
defihuntersdao.club <github@defihuntersdao.club>
2022-04-23 08:22:15 +0000

    prepare to preg

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,2 +1,36 @@
 <?php
+$d = __DIR__."/";
+
+$item = $_GET[item];
+
+$item = basename($item);
+
+$f = $item.".html";
+if(file_exists($f))
+{
+    $a = file_get_contents($f);
+    print $a;
+}
+else
+{
+$h = opendir($d);
+while($f = readdir($h))
+{
+    if($f == ".." || $f == ".")continue;
+
+    $tf = $d.$f;
+    if(is_dir($tf))continue;
+    $t = pathinfo($tf);
+    if($t[extension] == "html")
+    {
+	//$o = $f;
+	$l[$f] = "?item=$t[filename]";
+    }
+}
+//print_r($l);
+    foreach($l as $f=>$link)
+    {
+	print "<a href=$link>$f</a> \n";
+    }
+}
 ?>
